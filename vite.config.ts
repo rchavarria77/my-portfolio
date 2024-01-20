ad92d8370a3516b2844f96734b3368551d09b6c8
Roberto Chavarria <rchavarria77@outlook.com>
2024-01-19 23:24:32 -0600

    adding SEO configurations

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,25 @@
+// Dependencies
 import { defineConfig } from 'vite';
+import generateSitemap from 'vite-plugin-pages-sitemap';
+import Pages from 'vite-plugin-pages';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+const config = defineConfig({
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    Pages({
+      onRoutesGenerated: (routes) => {
+        generateSitemap({
+          hostname: 'http://localhost:3000/',
+          readable: true,
+          routes: [...routes],
+        });
+      },
+    }),
+  ],
 });
+
+export default config;
