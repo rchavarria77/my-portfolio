b1333d988abf372aa6a4a03b9fe0727d29b47990
Roberto Chavarria <rchavarria77@outlook.com>
2024-01-22 23:46:13 -0600

    fixing download cv button

--- src/components/introduction/Documentation.tsx
+++ src/components/introduction/Documentation.tsx
@@ -1,23 +1,46 @@
 // Components
-import { SocialPill } from '@/components/global-components';
+import { Button, SocialPill } from '@/components/global-components';
 
 // Icons
 import { DownloadIcon, MessageIcon } from '@/components/icons';
 
 export const Documentation = () => {
+  //#region functions
+  const handleDownload = () => {
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = 'blob';
+
+    xhr.onload = () => {
+      const blob = xhr.response;
+      const url = window.URL.createObjectURL(blob);
+
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = 'CV_Carlos_Roberto_Chavarria_Vega_EN.pdf'; // Set a default filename if not provided
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+
+      window.URL.revokeObjectURL(url);
+    };
+
+    xhr.open('GET', '/assets/cv.pdf');
+    xhr.send();
+  };
+  //#endregion
+
   return (
     <div className='flex flex-col items-center order-3 gap-4 pt-8 lg:items-start md:pt-0'>
-      <SocialPill
-        href='https://flowcv.me/roberto-chavarria/file-r4od9wkvn68h.pdf'
-        className='gap-1 hover:scale-110 min-w-max animate-pulse animate-infinite hover:animate-none'
-        ariaLabel='Download CV'
+      <Button
+        handleClick={handleDownload}
+        className='gap-1 bg-[transparent] border-none hover:scale-110 min-w-max animate-pulse animate-infinite hover:animate-none'
       >
         <DownloadIcon className='size-4 md:size-4 text-shark-300' />
         <p className='flex items-center text-sm text-shark-300'>
           <span className='hidden lg:flex'>Download &nbsp;</span>
           <span>CV</span>
         </p>
-      </SocialPill>
+      </Button>
       <SocialPill
         href='#contact'
         target='_self'
